a185a3ef516612acaefe37e8b78ba117c75e3de4
Ademar Vazquez <ademarvazquez53@gmail.com>
2026-04-18 05:12:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5229056519-da442",
+  "appId": "1:92376589106:web:580a1b65c67625e4ceed2b",
+  "apiKey": "AIzaSyA5TXjnPv8C5sdU6-nNfELrxfA5xKq3hz4",
+  "authDomain": "studio-5229056519-da442.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "92376589106"
+};
